560bac0e2d0ae23db72ae72522a611a48f4b3ba2
pc1407 <160670459+pc1407@users.noreply.github.com>
2024-06-24 00:13:03 +0530

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull pc1407/flask-app
+docker pull pc1407/flask-app:latest
 
 # Run the Docker image as a container
 docker run -d -p 5000:5000 pc1407/flask-app
